57a869e62d7c35b46a38302e88b7b52cedb88968
Peter Hansen <phansen@brainshark.com>
2020-06-07 19:06:09 -0400

    initial commit

---
+++ ExponentComputation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExponentComputation
+{
+    class Program
+    {
+        public static int ComputePower(int baseNumber, int exponent)
+        {
+            var result = 1;
+
+            for (var i = 0; i < exponent; i++)
+            {
+                result *= baseNumber;
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            var result = ComputePower(2, 8);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
